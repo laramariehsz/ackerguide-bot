d36f960093d98ff982862506da3cdb5b1214eb5a
laramariehsz <Lhessz@aol.com>
2025-07-16 17:15:09 +0200

    Testroute für /api/videos hinzugefügt

--- server.js
+++ server.js
@@ -1,22 +1,18 @@
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
-const { createClient } = require("webdav");
-
 dotenv.config();
+
 const app = express();
 app.use(cors());
 
 const PORT = process.env.PORT || 3000;
 
-
 app.get("/api/videos", (req, res) => {
   res.json({ status: "OK", message: "Test erfolgreich" });
 });
 
-
-app.listen(PORT, '0.0.0.0', () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log("📦 process.env.PORT:", process.env.PORT);
   console.log(`✅ API läuft unter http://0.0.0.0:${PORT}/api/videos`);
 });
-
